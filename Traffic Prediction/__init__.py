18ad55735841cc6e3fcc2a9d9c746227576e025e
NhuanTDBK <tranducnhuan1994@gmail.com>
2015-12-02 21:51:51 +0700

    Extract data

---
+++ Traffic Prediction/__init__.py
@@ -0,0 +1,25 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as pl
+import sklearn
+from sknn.mlp import Regressor, Layer
+import pickle
+from sknn.platform import cpu64
+raw_data = pd.read_csv("workload.csv");
+def get_training():
+ #   n_row = 578 
+ # group du lieu
+    
+    n_input = 10
+    data = raw_data["Utilization"]
+    n_row = data.shape[0]
+    print "Generate X_traing, y_traing"
+    print "X_training loading..."
+    X_training = np.asarray([[data[t-i-1] for i in range(0,n_input)]
+                 for t in np.arange(n_input,n_row)])
+    print "y_training loading..."
+    y_training = np.asarray(data[n_input:n_row])
+    n_sample2 = np.asarray([[data[t-i-1] for i in range(0,n_input)] for t in np.arange (289*400,289*410)])
+    print "y_test..."
+    n_test2 =  np.asarray([raw_data.ix[t][4] for t in np.arange(289*400+1,289*410+1)])
+    return X_training, y_training,n_sample2,n_test2
